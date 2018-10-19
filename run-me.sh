ac886df9966181bc55f7ef0c5d1e6829aaab9577
xiyinzhi <452964062@qq.com>
2018-10-18 20:22:18 -0400

    edit

--- run-me.sh
+++ run-me.sh
@@ -6,14 +6,6 @@
 
 # the weird "$@" syntax below means that whatever command-line
 # arguments are passed to run-me.sh will be passed again to the Python
-# program called hw1.py.
+# program called EncryptedIM.py.
 
 python -u EncryptedIM.py $@
-
-# but that's for python.  If you did it in C and you produced a program
-# called hw1, then it'd be more like:
-#   ./hw1 $@
-
-# but that's for C.  If you did it in Java and you produced a program
-# called Hw1.class, then it'd be more like:
-#   java Hw1 $@
